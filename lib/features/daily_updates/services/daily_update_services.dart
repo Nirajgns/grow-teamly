81bfd0ea70d936c665547b9b1800259228e54b98
Niraj Ganesh <nirajgns@gmail.com>
2022-06-29 12:16:14 +0545

    new origin

--- lib/features/daily_updates/services/daily_update_services.dart
+++ lib/features/daily_updates/services/daily_update_services.dart
@@ -6,7 +6,7 @@ import 'package:profileapp/core/network_client.dart';
 import 'package:profileapp/features/daily_updates/models/daily_update_model.dart';
 
 class DailyUpdateServices {
-Future<List<DailyUpdateModel>> fetchDailyUodates() async {
+Future<List<DailyUpdateModel>> fetchDailyUpdates() async {
     final http.Response? response =
         await NetworkClient().getRequest(path: ApiEndpoints.fetchDailyUpdate);
 
